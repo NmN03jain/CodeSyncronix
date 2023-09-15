c2dbcac883d85ea54d1a2c7be887ea4afb41370f
Pradyum Tahekar <pradyum.tahekar@gmail.com>
2023-09-15 10:57:08 +0530

    change in Editor file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Routes>
 
             <Route path='/' element={<Entry/>} />
-            <Route path='/Editor/:roomId' element={<Editor/>}  />
+            <Route path='/Editor/' element={<Editor/>}  />
         </Routes>
       </BrowserRouter>
 
