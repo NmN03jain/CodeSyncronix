8fd417340ec6cce5e21aadd4417cef3a7bd9300e
Pradyum Tahekar <pradyum.tahekar@gmail.com>
2023-09-17 14:43:47 +0530

    Connect Socket.io to Front-end

--- Client/src/components/Collaborate.js
+++ Client/src/components/Collaborate.js
@@ -1,27 +1,38 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:5000");
+
 
 const Collaborate = () => {
 
-    // Fetching the Data from backend and storing it on the backendData Variable
-    const [backendData, setBackendData] = useState([{}]);
+    //Using useState for getting what the user is typing on the code-editor
+    const [message, setMessage] = useState("");
+    const [messageReceived, setMessageReceived] = useState("");
+    const sendMessage = () => {
+        socket.emit("send-message", { message });
+    }
+    // we are using this useEffect to listen from the data 
+    //which is emitted by the server by "socket.emit("receive-message", data)";
     useEffect(() => {
-        fetch("/api").then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data);
-            }
-        )
-    }, []);
-
+        socket.on("receive-message", (data) => {
+            setMessageReceived(data.message);
+        });
+    }, [socket]);
 
     return (
         <>
             <div className="code-editor">
-                <pre contenteditable="true" class="code-box">
-                    {backendData.users}
+                <input placeholder="Message..." onChange={(event) => {
+                    setMessage(event.target.value);
+                }}/>
+                <button onClick={sendMessage}>Send</button>
+                
+                <pre contenteditable="true" class="code-box" >
+                    {messageReceived}
                 </pre>
+
             </div>
         </>
     )
--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,37 @@
 const express = require('express');
-const socket = require('socket.io');
+const {Server} = require('socket.io');
 const http = require('http');
-
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socket(server);
-
-app.get('/api', (req,res) => {
-    res.json({"users":"Pradyum"});
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
+    },
 });
 
-io.on('connection',(socket)=> {
-    console.log("user is connected");
+// io.on listen for the event
+io.on('connection',(socket) => {
+    console.log("User is connected");
+
+    //listining for the send-message event
+    socket.on("send-message", (data) => {
+        // console.log(data);
+        socket.broadcast.emit("receive-message", data);
+    })
+})
+
+
+server.listen(5000, ()=>{
+    console.log("Server Started on PORT: 5000");
 })
 
 
 
 
 
-app.listen(5000, ()=>{
-    console.log("Server started on PORT: 5000");
-});
+
+
+
