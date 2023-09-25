09379f4f5cadb02e0a91f9efdbda3faac90a63f8
Pradyum Tahekar <pradyum.tahekar@gmail.com>
2023-09-25 12:27:51 +0530

    copied new repo

--- Client/src/components/Entry.js
+++ Client/src/components/Entry.js
@@ -1,70 +1,67 @@
-import React, { useState } from 'react';
-import { v4 } from "uuid";
+import React from 'react'
+import {v4} from "uuid";
+import { useState } from 'react';
 import toast from "react-hot-toast";
 import { useNavigate } from 'react-router-dom';
 
-
 const Entry = () => {
-
   const navigate = useNavigate();
   const [roomId , setRoomId] = useState('');
   const [userName , setUserName] = useState('');
+  
   const CreateRoom = (e)=>{
     e.preventDefault();
     const id = v4();
     setRoomId(id);
-    toast.success("New Room Created");
-  };
+    toast.success("New Room Created ")
+  }
+  const JoinRoom = ()=>{
 
-  const JoinRoom = () => {
-
-    if (!roomId || !userName) {
-      toast.error("UserName and Room Id is required !");
+    if(!roomId || !userName){
+      toast.error("UserName and Room Id is required !")
     }
     else{
     navigate(`/Collaborate/${roomId}`,{
       state:{
-        userName, 
+        userName, roomId,
       },
     })
   }
   }
 
-  const ForEnter = (event) => {
-    if (event.code === "Enter") {
+  const ForEnter = (e)=>{
+    if(e.code==="Enter"){
       JoinRoom();
     }
-  };
+  }  
 
   return (
     <>
       <div className="Home">
         <div className='Form'>
-          <h1 className='heading'>CodeSyncronix</h1>
+            <h1 className='heading'>CodeSyncronix</h1>
           <div className='inputs'>
-            <input
-              className='inputfield'
-              type='text'
-              placeholder='USER-NAME'
-              value={userName}
-              onChange={(event) => { setUserName(event.target.value) }}
-              onKeyUp={ForEnter}
-            />
-            <input
-              className='inputfield'
-              type='text'
-              placeholder='ROOM-ID'
-              onChange={(event) => { setRoomId(event.target.value) }}
-              value={roomId}
-              onKeyUp={ForEnter}
-            />
-            <button onClick={JoinRoom} className='but'> Join Room </button>
-            <p className='hel'>Want to create new Room-ID ? &nbsp; <a onClick={CreateRoom} href="_">Create ID</a> </p>
+
+            <input className='inputfield' type='text' placeholder='USER-NAME' value={userName} onChange={(e)=>{setUserName(e.target.value)}} onKeyUp={ForEnter} />
+
+            <input className='inputfield' type='text' placeholder='ROOM-ID' onChange={(e)=>{setRoomId(e.target.value)}} value={roomId} onKeyUp={ForEnter} />
+
+            <button onClick={JoinRoom} className='but'> Join RooM </button>
+
+            <p className='hel'>IF You Want to create Room &nbsp; <a onClick={CreateRoom} href="_">Create Room ID</a> </p>
           </div>
         </div>
+        {/* Company Name © DeepAI 2021 */}
+        <footer>
+                <h4 className='y'>
+                  Naman &nbsp;&&nbsp; Pradyumn © CodeSyncronix 2023
+                </h4>
+            </footer>
+        
       </div>
-    </>
-  );
-};
 
-export default Entry;
+    </>
+  )
+}
+
+export default Entry
