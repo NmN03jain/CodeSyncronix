2fcd0a91572a1779b55269003f3ddd047bc0f6bd
Pradyum Tahekar <pradyum.tahekar@gmail.com>
2023-09-25 11:05:15 +0530

    ignore it

--- Client/src/components/Collaborate.js
+++ Client/src/components/Collaborate.js
@@ -1,84 +1,253 @@
-import React from "react";
-import './Collaborate.css';
-import { useLocation } from "react-router-dom";
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
-import Editor from '@monaco-editor/react';
-import { useRef } from "react";
+// import React from "react";
+// import './Collaborate.css';
+// import { useLocation } from "react-router-dom";
+// import io from "socket.io-client";
+// import { useEffect, useState } from "react";
+// import Editor from '@monaco-editor/react';
+// import { useRef } from "react";
+// import Axios from 'axios';
+// // import spinner from './spinner.svg';
 
-const Collaborate = () => {
-    const location = useLocation();
-    const user = location.state.userName;
-    const roomId = location.state.roomId;
-    const [socket, setSocket] = useState(null);
-    const [editorContent, setEditorContent] = useState("// some comment"); // State to store editor content
+// const Collaborate = () => {
+//     const location = useLocation();
+//     const user = location.state.userName;
+//     const roomId = location.state.roomId;
+//     const [socket, setSocket] = useState(null);
+//     const [editorContent, setEditorContent] = useState("// some comment"); // State to store editor content
+    
+//     const [userInput, setUserInput] = useState("");
+//     const [userOutput, setUserOutput] = useState("");
+//     const [loading, setLoading] = useState(false);
+//     // Create a ref to the Monaco Editor instance
+//     const editorRef = useRef(null);
 
-    // Create a ref to the Monaco Editor instance
-    const editorRef = useRef(null);
+//     useEffect(() => {
+//         const newSocket = io.connect("http://localhost:5000", {
+//             query: { roomId },
+//         });
 
-    useEffect(() => {
-        const newSocket = io.connect("http://localhost:5000", {
-            query: { roomId },
-        });
+//         newSocket.on("connect", () => {
+//             newSocket.emit("join-room", roomId);
+//         });
 
-        newSocket.on("connect", () => {
-            newSocket.emit("join-room", roomId);
-        });
+//         setSocket(newSocket);
 
-        setSocket(newSocket);
+//         // Function to handle incoming editor content updates
+//         const handleEditorContentUpdate = (data) => {
+//             // Update the editor content state
+//             setEditorContent(data.newText);
+//         };
 
-        // Function to handle incoming editor content updates
-        const handleEditorContentUpdate = (data) => {
-            // Update the editor content state
-            setEditorContent(data.newText);
-        };
+//         // Listen for changes in the editor content from other clients
+//         newSocket.on("receive-editor-content", handleEditorContentUpdate);
 
-        // Listen for changes in the editor content from other clients
-        newSocket.on("receive-editor-content", handleEditorContentUpdate);
+//         return () => {
+//             if (socket) {
+//                 socket.disconnect();
+//             }
+//         };
+//     }, [roomId, socket]);
 
-        return () => {
-            if (socket) {
-                socket.disconnect();
-            }
-        };
-    }, [roomId]);
+//     // Function to handle changes in the Monaco Editor
+//     const handleEditorChange = (value, event) => {
+//         // Update the editor content state
+//         setEditorContent(value);
 
-    // Function to handle changes in the Monaco Editor
-    const handleEditorChange = (value, event) => {
-        // Update the editor content state
-        setEditorContent(value);
+//         // Emit a socket event to update the editor content for other clients
+//         socket.emit("editor-content-update", { newText: value, roomId });
+//     };
 
-        // Emit a socket event to update the editor content for other clients
-        socket.emit("editor-content-update", { newText: value, roomId });
-    };
+//     function compile() {
+//         setLoading(true);
+//         if (editorContent === ``) {
+//             return
+//         }
 
-    return (
-        <>
-            <div className="main-container">
-                <div className="side-panel">
-                    <div className="Code-Syncronix">
-                        <h2>Code-Syncronix</h2>
-                    </div>
-                    <ul>
-                        <li>{user}</li>
-                    </ul>
-                </div>
-                <div className="editor">
-                    <Editor
-                        height="90vh"
-                        defaultLanguage="javascript"
-                        value={editorContent} // Set the editor content from state
-                        onChange={handleEditorChange} // Handle editor changes
-                        // Pass in the editor reference to get the editor instance
-                        onMount={(editor, monaco) => {
-                            // Save a reference to the editor instance
-                            editorRef.current = editor;
-                        }}
-                    />
-                </div>
-            </div>
-        </>
-    );
-};
+//         // Post request to compile endpoint
+//         Axios.post(`http://localhost:6000/compile`, {
+//             code: editorContent,
+//             language: "javascript",
+//             input: userInput
+//         }).then((res) => {
+//             setUserOutput(res.data.output);
+//         }).then(() => {
+//             setLoading(false);
+//         })
+//     }
 
-export default Collaborate;
+//     function clearOutput() {
+//         setUserOutput("");
+//     }
+
+//     return (
+//         <>
+//             <div className="main-container">
+//                 <div className="side-panel">
+//                     <div className="Code-Syncronix">
+//                         <h2>Code-Syncronix</h2>
+//                     </div>
+//                     <ul>
+//                         <li>{user}</li>
+//                     </ul>
+//                 </div>
+//                 <div className="editor">
+//                     <Editor 
+//                         width="700px"
+//                         height="90vh"
+//                         defaultLanguage="javascript"
+//                         value={editorContent} // Set the editor content from state
+//                         onChange={handleEditorChange} // Handle editor changes
+//                         // Pass in the editor reference to get the editor instance
+//                         onMount={(editor, monaco) => {
+//                             // Save a reference to the editor instance
+//                             editorRef.current = editor;
+//                         }}
+//                     />
+//                     <button className="run-btn" onClick={() => compile()}>
+//                         Run
+//                     </button>
+//                 </div>
+//                 <div className="right-container">
+//                     <h4>Input:</h4>
+//                     <div className="input-box">
+//                         <textarea id="code-inp" onChange=
+//                             {(e) => setUserInput(e.target.value)}>
+//                         </textarea>
+//                     </div>
+//                     <h4>Output:</h4>
+//                     {loading ? (
+//                         <div className="spinner-box">
+//                             <h1>Loading</h1>
+//                         </div>
+                        
+//                     ) : (
+//                         <div className="output-box">
+//                             <pre>{userOutput}</pre>
+//                             <button onClick={() => { clearOutput() }}
+//                                 className="clear-btn">
+//                                 Clear
+//                             </button>
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+        
+//         </>
+//     );
+// };
+
+// export default Collaborate;
+
+
+import { useState } from 'react';
+import './app.css';
+import Editor from "@monaco-editor/react";
+// import Navbar from './Components/Navbar';
+import Navbar from './navbar';
+import Axios from 'axios';
+// import spinner from './spinner.svg';
+
+function App() {
+
+	// State variable to set users source code
+	const [userCode, setUserCode] = useState(``);
+
+	// State variable to set editors default language
+	const [userLang, setUserLang] = useState("python");
+
+	// State variable to set editors default theme
+	const [userTheme, setUserTheme] = useState("vs-dark");
+
+	// State variable to set editors default font size
+	const [fontSize, setFontSize] = useState(20);
+
+	// State variable to set users input
+	const [userInput, setUserInput] = useState("");
+
+	// State variable to set users output
+	const [userOutput, setUserOutput] = useState("");
+
+	// Loading state variable to show spinner
+	// while fetching data
+	const [loading, setLoading] = useState(false);
+
+	const options = {
+		fontSize: fontSize
+	}
+
+	// Function to call the compile endpoint
+	function compile() {
+		setLoading(true);
+		if (userCode === ``) {
+			return
+		}
+
+		// Post request to compile endpoint
+		Axios.post(`http://localhost:8000/compile`, {
+			code: userCode,
+			language: userLang,
+			input: userInput
+		}).then((res) => {
+			setUserOutput(res.data.output);
+		}).then(() => {
+			setLoading(false);
+		})
+	}
+
+	// Function to clear the output screen
+	function clearOutput() {
+		setUserOutput("");
+	}
+
+	return (
+		<div className="App">
+			<Navbar
+				userLang={userLang} setUserLang={setUserLang}
+				userTheme={userTheme} setUserTheme={setUserTheme}
+				fontSize={fontSize} setFontSize={setFontSize}
+			/>
+			<div className="main">
+				<div className="left-container">
+					<Editor
+						options={options}
+						height="calc(100vh - 50px)"
+						width="100%"
+						theme={userTheme}
+						language={userLang}
+						defaultLanguage="python"
+						defaultValue="# Enter your code here"
+						onChange={(value) => { setUserCode(value) }}
+					/>
+					<button className="run-btn" onClick={() => compile()}>
+						Run
+					</button>
+				</div>
+				<div className="right-container">
+					<h4>Input:</h4>
+					<div className="input-box">
+						<textarea id="code-inp" onChange=
+							{(e) => setUserInput(e.target.value)}>
+						</textarea>
+					</div>
+					<h4>Output:</h4>
+					{loading ? (
+						<div className="spinner-box">
+                            {/* <img src={spinner} alt="Loading..." /> */}
+                            <p>loading..</p>
+                        </div>
+					) : (
+						<div className="output-box">
+							<pre>{userOutput}</pre>
+							<button onClick={() => { clearOutput() }}
+								className="clear-btn">
+								Clear
+							</button>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
--- Server/server.js
+++ Server/server.js
@@ -1,46 +1,129 @@
-const express = require('express');
-const { Server } = require('socket.io');
-const http = require('http');
-const cors = require('cors');
+// const express = require('express');
+// const { Server } = require('socket.io');
+// const http = require('http');
+// const cors = require('cors');
 
+// const app2 = express();
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//     cors: {
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST"],
+//     },
+// });
+
+// app2.post("/compile", (req, res) => {
+//     //getting the required data from the request
+//     let code = req.body.code;
+//     let language = req.body.language;
+//     let input = req.body.input;
+
+//     if (language === "python") {
+//         language = "py"
+//     }
+
+//     let data = ({
+//         "code": code,
+//         "language": language,
+//         "input": input
+//     });
+//     let config = {
+//         method: 'post',
+//         url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         data: data
+//     };
+//     //calling the code compilation API
+//     Axios(config)
+//         .then((response) => {
+//             res.send(response.data)
+//             console.log(response.data)
+//         }).catch((error) => {
+//             console.log(error);
+//         });
+// })
+
+// io.on("connection", (socket) => {
+//     console.log("A user connected", socket.id);
+
+//     socket.on("join-room", (roomId) => {
+//         socket.join(roomId);
+//         console.log(`User joined room: ${roomId}`);
+//     });
+
+//     socket.on("editor-content-update", (data) => {
+//         // Broadcast the updated text to all clients in the same room
+//         io.to(data.roomId).emit("receive-editor-content", {
+//             newText: data.newText,
+//         });
+//     });
+
+//     socket.on("disconnect", () => {
+//         console.log("A user disconnected");
+//     });
+// });
+
+// app2.listen(6000,() =>{
+//     console.log("App2 Server Started on PORT: 6000");
+// })
+
+// server.listen(5000, () => {
+//     console.log("Server Started on PORT: 5000");
+// });
+
+
+
+
+
+
+
+const express = require("express");
+const cors = require("cors");
+const Axios = require("axios");
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
-});
-
-io.on("connection", (socket) => {
-    console.log("A user connected", socket.id);
-
-    socket.on("join-room", (roomId) => {
-        socket.join(roomId);
-        console.log(`User joined room: ${roomId}`);
+const PORT = 8000;
+ 
+app.use(cors());
+app.use(express.json());
+ 
+app.post("/compile", (req, res) => {
+    //getting the required data from the request
+    let code = req.body.code;
+    let language = req.body.language;
+    let input = req.body.input;
+ 
+    if (language === "python") {
+        language = "py"
+    }
+ 
+    let data = ({
+        "code": code,
+        "language": language,
+        "input": input
     });
-
-    socket.on("editor-content-update", (data) => {
-        // Broadcast the updated text to all clients in the same room
-        io.to(data.roomId).emit("receive-editor-content", {
-            newText: data.newText,
+    let config = {
+        method: 'post',
+        url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    //calling the code compilation API
+    Axios(config)
+        .then((response) => {
+            console.log(response.data);
+            res.send(response.data)
+            console.log(response.data)
+        }).catch((error) => {
+            // console.log(error);
+            console.log("error");
         });
-    });
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
+})
+ 
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
-
-
-server.listen(5000, () => {
-    console.log("Server Started on PORT: 5000");
-});
-
-
-
-
-
-
-
-
